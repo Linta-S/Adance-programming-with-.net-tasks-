4303d21c461d0c24645125c37500c738e93f4c09
Linta-S <sahara.lintaa@gmail.com>
2022-02-09 03:09:53 +0600

    Lab Task 2

---
+++ ProductList/ProductList/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductList.Models;
+
+namespace ProductList.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<product> productList = new List<product>();   //class
+            for(int i=0;i<10;i++)
+            {
+                var p = new product()
+                {
+                    Id = i + 1,
+                    Name = "Product " + (i + 1),
+                    Details = "This is a Demo product for people who wants best servies at low cost.",
+                    Price = 100 * i,
+
+                };
+
+                productList.Add(p);
+            }
+            return View(productList);
+        }
+    }
+}
